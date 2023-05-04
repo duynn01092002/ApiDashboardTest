c5405b2547d9faec38ddca69d262f7dbb821562e
duynn01092002 <nguyenngocduy12a2@gmail.com>
2023-05-04 16:42:40 +0700

    Thêm các negative testcase của phần login

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+S
+#src/test/java/user_manage_test.java,4\9\49afdd5364909dabae366e070302ff3131e112bd
+O
+src/test/java/profile_test.java,e\8\e8e09c531424d7533bc21f193e9d697dfbbe55f7
+4
+.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+N
+src/test/java/signup_test.java,e\9\e913732efcfcfbf9c0714a3e97861c085652c611
+:
+
+Dockerfile,6\6\6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+K
+src/test/java/api_test.java,3\6\366e495a7067bfb55d8acdcdc882a7bfb3b36daa
--- src/test/java/login_test.java
+++ src/test/java/login_test.java
@@ -1,5 +1,6 @@
 import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -27,12 +28,60 @@ public class login_test {
     }
 
     @Test(priority = 2)
-    public void testNoCredentialLogin() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+    public void testNoCredentialLogin() throws MalformedURLException {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get("https://www.google.com");
-        System.out.println(driver.getCurrentUrl());
+        driver.get(login_url);
+        driver.findElement(By.name("username")).sendKeys("");
+        driver.findElement(By.name("password")).sendKeys("");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement alert_message1 = driver.findElement(By.xpath("//span[@id='errorMsg1']"));
+        WebElement alert_message2 = driver.findElement(By.xpath("//span[@id='errorMsg2']"));
+        assert alert_message1.getText().contains("tên đăng nhập") && alert_message2.getText().contains("mật khẩu");
+        driver.quit();
+    }
+
+    @Test(priority = 3)
+    public void testRoleIsChecked() throws MalformedURLException {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        URL url = new URL(selenium_grid_url);
+        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
+        driver.get(login_url);
+        driver.findElement(By.name("username")).sendKeys("ngocduy");
+        driver.findElement(By.name("password")).sendKeys("123Duy");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement alert_role_is_checked = driver.findElement(By.xpath("//div[@class='alert alert-danger']"));
+        assert alert_role_is_checked.getText().contains("checkbox Role");
+        driver.quit();
+    }
+    @Test(priority = 4)
+    public void testInvalidCredential() throws MalformedURLException {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        URL url = new URL(selenium_grid_url);
+        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
+        driver.get(login_url);
+        driver.findElement(By.name("username")).sendKeys("ngocduy");
+        driver.findElement(By.name("password")).sendKeys("123D");
+        driver.findElement(By.name("admin")).click();
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement alert_invalid_credential = driver.findElement(By.xpath("//div[@class='alert alert-danger']"));
+        assert alert_invalid_credential.getText().contains("Invalid");
+        driver.quit();
+    }
+
+    @Test(priority = 5)
+    public void testAccessDenied() throws MalformedURLException {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        URL url = new URL(selenium_grid_url);
+        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
+        driver.get(login_url);
+        driver.findElement(By.name("username")).sendKeys("ngocduy");
+        driver.findElement(By.name("password")).sendKeys("123Duy");
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement alert_access_denied = driver.findElement(By.xpath("//div[@class='alert alert-danger']"));
+        assert alert_access_denied.getText().contains("Access");
         driver.quit();
     }
 }
