014ab5a0cc7f19c6f6829485422e2883a7ccd536
duynn01092002 <nguyenngocduy12a2@gmail.com>
2023-05-05 00:38:34 +0700

    fix signup test

--- src/test/java/signup_test.java
+++ src/test/java/signup_test.java
@@ -26,32 +26,33 @@ public class signup_test {
         DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.manage().window().maximize();
         driver.get(signup_url);
         driver.findElement(By.name("username")).sendKeys("nhom06");
         driver.findElement(By.name("password")).sendKeys("Nhom06");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name1");
+        driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        driver.findElement(By.name("address")).sendKeys("Test Address");
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
         driver.findElement(By.name("email")).sendKeys("nhom06@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-admin")).click();
+        driver.findElement(By.id("radio-normal")).click();
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-
         driver.quit();
     }
+
     @Test(priority = 2)
     public void TestSignUpDuplicateUsername() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("test");
+        driver.findElement(By.name("username")).sendKeys("nhom06");
         driver.findElement(By.name("password")).sendKeys("Test123");
         driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.findElement(By.name("address")).sendKeys("Test Address");
         driver.findElement(By.name("email")).sendKeys("Test123@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
@@ -63,65 +64,22 @@ public class signup_test {
         wait.until(ExpectedConditions.visibilityOfElementLocated(alertLocator));
         WebElement alert_checkCredentials = driver.findElement(alertLocator);
         String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
         assert alert_text.equals("Username đã tồn tại");
         driver.quit();
     }
-    @Test(priority = 3)
-    public void TestSignUpEmptyUsername() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("");
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name");
-        driver.findElement(By.name("age")).sendKeys("1");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
-        driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg1"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền tên đăng nhập");
-        driver.quit();
-    }
-    @Test(priority = 4)
-    public void TestSignUpEmptyPassword() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name");
-        driver.findElement(By.name("age")).sendKeys("1");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
-        driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg2"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền mật khẩu");
-        driver.quit();
-    }
     @Test(priority = 5)
     public void TestSignUpShortPassword() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(signup_url);
         driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("password")).sendKeys("Te12");
         driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.findElement(By.name("address")).sendKeys("Test Address");
         driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
@@ -133,13 +91,13 @@ public class signup_test {
         wait.until(ExpectedConditions.visibilityOfElementLocated(alertLocator));
         WebElement alert_checkCredentials = driver.findElement(alertLocator);
         String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
         assert alert_text.equals("Mật khẩu phải từ 6 đến 20 ký tự");
         driver.quit();
     }
+
     @Test(priority = 6)
     public void TestSignUpUpperAlphabetPassword() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(signup_url);
@@ -147,6 +105,8 @@ public class signup_test {
         driver.findElement(By.name("password")).sendKeys("test123");
         driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.findElement(By.name("address")).sendKeys("Test Address");
         driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
@@ -158,13 +118,13 @@ public class signup_test {
         wait.until(ExpectedConditions.visibilityOfElementLocated(alertLocator));
         WebElement alert_checkCredentials = driver.findElement(alertLocator);
         String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
         assert alert_text.equals("Mật khẩu phải chứa ít nhất một chữ hoa");
         driver.quit();
     }
+
     @Test(priority = 7)
     public void TestSignUpLowerAlphabetPassword() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(signup_url);
@@ -172,6 +132,8 @@ public class signup_test {
         driver.findElement(By.name("password")).sendKeys("T12345");
         driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.findElement(By.name("address")).sendKeys("Test Address");
         driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
@@ -183,13 +145,13 @@ public class signup_test {
         wait.until(ExpectedConditions.visibilityOfElementLocated(alertLocator));
         WebElement alert_checkCredentials = driver.findElement(alertLocator);
         String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
         assert alert_text.equals("Mật khẩu phải chứa ít nhất một chữ thường");
         driver.quit();
     }
+
     @Test(priority = 8)
     public void TestSignUpNumberPassword() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(signup_url);
@@ -197,10 +159,12 @@ public class signup_test {
         driver.findElement(By.name("password")).sendKeys("Testpass");
         driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.findElement(By.name("address")).sendKeys("Test Address");
         driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
+        driver.findElement(By.xpath("//input[@id='radio-normal']")).click();
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -208,114 +172,24 @@ public class signup_test {
         wait.until(ExpectedConditions.visibilityOfElementLocated(alertLocator));
         WebElement alert_checkCredentials = driver.findElement(alertLocator);
         String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
         assert alert_text.equals("Mật khẩu phải chứa ít nhất một số");
         driver.quit();
     }
-    @Test(priority = 9)
-    public void TestSignUpEmptyFullname() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("fullname")).sendKeys("");
-        driver.findElement(By.name("age")).sendKeys("1");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
-        driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg3"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền tên");
-        driver.quit();
-    }
-    @Test(priority = 10)
-    public void TestSignUpEmptyAge() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name");
-        driver.findElement(By.name("age")).sendKeys("");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
-        driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg4"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền tuổi");
-        driver.quit();
-    }
-    @Test(priority = 11)
-    public void TestSignUpEmptyAddress() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name");
-        driver.findElement(By.name("age")).sendKeys("1");
-        driver.findElement(By.name("address")).sendKeys("");
-        driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
-        driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg5"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền địa chỉ");
-        driver.quit();
-    }
-    @Test(priority = 12)
-    public void TestSignUpEmptyEmail() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name");
-        driver.findElement(By.name("age")).sendKeys("1");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("");
-        driver.findElement(By.name("avatar")).sendKeys("http://link");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg6"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền email");
-        driver.quit();
-    }
     @Test(priority = 13)
     public void TestSignUpDuplicateEmail() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL(selenium_grid_url);
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest12");
+        driver.findElement(By.name("username")).sendKeys("UserTest123");
         driver.findElement(By.name("password")).sendKeys("Test123");
         driver.findElement(By.name("fullname")).sendKeys("Test Name");
         driver.findElement(By.name("age")).sendKeys("1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("TestGmail@gmail.com");
+        driver.findElement(By.name("email")).sendKeys("nhom06@gmail.com");
         driver.findElement(By.name("avatar")).sendKeys("http://link");
         driver.findElement(By.id("radio-normal")).click();
         driver.findElement(By.xpath("//button[@type='submit']")).click();
@@ -325,31 +199,7 @@ public class signup_test {
         wait.until(ExpectedConditions.visibilityOfElementLocated(alertLocator));
         WebElement alert_checkCredentials = driver.findElement(alertLocator);
         String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
         assert alert_text.equals("Email đã được sử dụng");
-        driver.quit();
-    }
-    @Test(priority = 14)
-    public void TestSignUpEmptyAvatar() throws InterruptedException, MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(signup_url);
-        driver.findElement(By.name("username")).sendKeys("UserTest");
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("fullname")).sendKeys("Test Name");
-        driver.findElement(By.name("age")).sendKeys("1");
-        driver.findElement(By.name("address")).sendKeys("Test Address");
-        driver.findElement(By.name("email")).sendKeys("Email@gmail.com");
-        driver.findElement(By.name("avatar")).sendKeys("");
-        driver.findElement(By.id("radio-normal")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        Thread.sleep(1000);
-        WebElement alert_checkCredentials = driver.findElement(By.id("errorMsg7"));
-        String alert_text = alert_checkCredentials.getText();
-        System.out.println(alert_text);
-        assert alert_text.equals("Vui lòng điền url images");
         driver.quit();
     }
 }
