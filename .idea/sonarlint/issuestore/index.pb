c6ea9cb97cefb3be34d2e0dccae23e6126a7d97b
duynn01092002 <nguyenngocduy12a2@gmail.com>
2023-05-20 16:59:05 +0700

    Modify dataFromApiTest -> overviewDataTest

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -4,7 +4,3 @@
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 B
 docker-compose.yml,3\5\35b8c13cf2eb2a194eada000eb310d65aed53b2a
-N
-src/test/java/profileTest.java,a\7\a790aecef460112bb4e65fe12b150d8ccb2fa135
-Q
-!src/test/java/userManageTest.java,0\3\03d4365bd596fa485b964a7d95b4f66ebac12da6
--- src/test/java/overviewDataTest.java
+++ src/test/java/overviewDataTest.java
@@ -10,11 +10,10 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class dataFromApiTest {
+public class overviewDataTest {
     private final Dotenv dotenv = Dotenv.load();
     private final String selenium_grid_url = dotenv.get("SELENIUM_GRID_URL");
     private final String home_url = dotenv.get("HOME_URL");
@@ -63,44 +62,8 @@ public class dataFromApiTest {
             }
         }
     }
+
     @Test(priority = 1)
-    public void testTopStatusOK() throws MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.chrome();
-        URL url = new URL(selenium_grid_url);
-        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-        driver.get(login_url);
-        driver.findElement(By.name("username")).sendKeys("nhom06");
-        driver.findElement(By.name("password")).sendKeys("Nhom06");
-        driver.findElement(By.name("admin")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-        List<WebElement> listStatusOK = driver.findElements(By.xpath("//p[contains(text(),'items')]"));
-        List<WebElement> listHOItem = driver.findElements(By.xpath("//p[contains(text(),'/HO')]"));
-        JSONObject responeJson = getData();
-        List<String> listStatus = new ArrayList<>();
-        List<String> listHO = new ArrayList<>();
-        for (WebElement statusOK : listStatusOK) {
-            listStatus.add(statusOK.getText());
-        }
-        for (WebElement ho : listHOItem) {
-            listHO.add(ho.getText());
-        }
-        boolean flag = true;
-        for (int i = 0;i < listHO.size();i++) {
-            int item_count = 0;
-            JSONObject HO_data = responeJson.getJSONObject(listHO.get(i));
-            for (String key : HO_data.keySet()) {
-                JSONObject item_data = HO_data.getJSONObject(key);
-                if (item_data.getString("status").equals("ok")) {
-                    item_count++;
-                }
-            }
-            if (!listStatus.get(i).equals(item_count + " items")) {
-                flag = false;
-            }
-        }
-        assert flag == true;
-    }
-    @Test(priority = 2)
     public void testTotal_HO() throws MalformedURLException {
         DesiredCapabilities dc = new DesiredCapabilities();
         dc = DesiredCapabilities.chrome();
@@ -108,9 +71,8 @@ public class dataFromApiTest {
 
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(login_url);
-        driver.findElement(By.name("username")).sendKeys("nhom06");
-        driver.findElement(By.name("password")).sendKeys("Nhom06");
-        driver.findElement(By.name("admin")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         List<WebElement> totalHOWeb = driver.findElements(By.xpath("//h3[@class='text-bold mb-10']"));
         String totalHOText = totalHOWeb.get(0).getText(); // lấy giá trị text của thẻ h3
@@ -120,7 +82,7 @@ public class dataFromApiTest {
         String expectedTotalHO  = String.valueOf(responeJson.length());
         assert expectedTotalHO.equals(actualTotalHO);
     }
-    @Test(priority = 3)
+    @Test(priority = 2)
     public void testTotal_children_in_HO() throws MalformedURLException {
         getDataFromAPI();
         DesiredCapabilities dc = new DesiredCapabilities();
@@ -129,9 +91,8 @@ public class dataFromApiTest {
 
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(login_url);
-        driver.findElement(By.name("username")).sendKeys("nhom06");
-        driver.findElement(By.name("password")).sendKeys("Nhom06");
-        driver.findElement(By.name("admin")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         List<WebElement> webElements = driver.findElements(By.xpath("//h3[@class='text-bold mb-10']"));
         String totalChildrenInHOText = webElements.get(1).getText(); // lấy giá trị text của thẻ h3
@@ -140,7 +101,7 @@ public class dataFromApiTest {
         String expectedChildrenInHO = String.valueOf(totalItemInHO);
         assert expectedChildrenInHO.equals(actualChildrenInHO);
     }
-    @Test(priority = 4)
+    @Test(priority = 3)
     public void testTotal_Status_OK() throws MalformedURLException {
         DesiredCapabilities dc = new DesiredCapabilities();
         dc = DesiredCapabilities.chrome();
@@ -148,9 +109,8 @@ public class dataFromApiTest {
 
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(login_url);
-        driver.findElement(By.name("username")).sendKeys("nhom06");
-        driver.findElement(By.name("password")).sendKeys("Nhom06");
-        driver.findElement(By.name("admin")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         List<WebElement> webElements = driver.findElements(By.xpath("//h3[@class='text-bold mb-10']"));
         String totalStatusOkText = webElements.get(2).getText(); // lấy giá trị text của thẻ h3
@@ -160,7 +120,7 @@ public class dataFromApiTest {
         String expectedTotalStatusOk = String.valueOf(totalStatusOK);
         assert expectedTotalStatusOk.equals(actualTotalStatusOk);
     }
-    @Test(priority = 5)
+    @Test(priority = 4)
     public void testTotal_Status_Fail() throws MalformedURLException {
         DesiredCapabilities dc = new DesiredCapabilities();
         dc = DesiredCapabilities.chrome();
@@ -168,9 +128,8 @@ public class dataFromApiTest {
 
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get(login_url);
-        driver.findElement(By.name("username")).sendKeys("nhom06");
-        driver.findElement(By.name("password")).sendKeys("Nhom06");
-        driver.findElement(By.name("admin")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         List<WebElement> webElements = driver.findElements(By.xpath("//h3[@class='text-bold mb-10']"));
         String totalStatusFailText = webElements.get(3).getText(); // lấy giá trị text của thẻ h3
